94efdffcf19748777432ed2dec7760f9e897d258
Nick <nick@hattwick.com>
2016-10-17 12:29:37 -0500

    Add existing file

--- splittest/setup.py
+++ splittest/setup.py
@@ -2,11 +2,6 @@ from random import shuffle
 from player import player
 
 def setup():
-    p1 = player('P1')
-    p2 = player('P2')
-
-    players = (p1, p2)
-
     for p in players:
         p.count = 0
         shuffle(p.deck)
