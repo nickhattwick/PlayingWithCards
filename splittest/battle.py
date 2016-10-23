1f74278e510cba45b338f66ccfe72eefd54cf7b9
Nick <nick@hattwick.com>
2016-10-23 01:53:00 -0500

    Add existing file

---
+++ splittest/battle.py
@@ -0,0 +1,9 @@
+def battle(x,y):
+    if x > y:
+        #pop y from controller's field
+        #Append to controller's discard pile
+    elif x < y:
+        #pop x from controller's field
+        #append x to controller's discard pile
+    else:
+        #both effects from above
