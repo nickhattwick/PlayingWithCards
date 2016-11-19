7d405989284435c25535aa28ed05187e1e718e34
Nick <nick@hattwick.com>
2016-11-19 11:48:25 -0600

    Add existing file

--- logging.py
+++ logging.py
@@ -1,11 +1,12 @@
 def game_log(func):
-    def inner():
-        func()
-        with open("game_log.txt", 'w') as f:
-            f.write(self.name, "s hand: ", self.hand)
-            f.write(self.name, "s field: ", self.field)
-            f.write(self.opponent.name, "s field: ", self.opponent.field)
-            f.write(self.name, "s LP: ", self.life)
-            f.write(self.opponent.name, "s LP: ", self.opponent.life)
-            f.write(self.lands)
+    def inner(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        with open("game_log.txt", 'a') as f:
+            f.write("Called " + func.__name__)
+            f.write(self.name + "s hand: " + str(self.hand) + "\n")
+            f.write(self.name + "s field: " + str(self.field) + "\n")
+            f.write(self.opponent.name + "s field: " + str(self.opponent.field) + "\n")
+            f.write(self.name + "s LP: " + str(self.life) + "\n")
+            f.write(self.opponent.name + "s LP: " + str(self.opponent.life) + "\n")
+            f.write(str(self.lands))
     return(inner)
