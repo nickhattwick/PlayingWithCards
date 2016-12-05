4b1bed0b7701bcd882c4e946bbb153294f5fc40e
Nick <nick@hattwick.com>
2016-12-04 22:37:57 -0600

    Add exisitng file

--- jsonlog.py
+++ jsonlog.py
@@ -1,13 +1,26 @@
 import json
 
+all_results = {}
+players = {}
+turn = 0
+
 def results_log(func):
     def inner(self, *args, **kwargs):
         func(self, *args, **kwargs)
-        game = {"W": winner.name, "L": loser.name}
-        with open("results.json", 'a') as results:
-            json.dump(game, results)
+        this_game_results = {"W": winner.name, "L": loser.name}
+        all_results[game_id] = this_game_result
+        with open("results.json".format, 'a') as results:
+            json.dump(all_results, results)
 
-def moves_log(func):
-    def inner(self, *args, **kwargs):
-        func(self, *args, **kwargs)
-        moves = {}
+def setup_log(firstplayer, secondplayer):
+    players.append(firstplayer.name, secondplayer.name)
+
+def turns_log(turnplayer):
+    turn.player = turnplayer
+
+
+
+
+#3 dictionaries
+#game results dictionary - game#, winner, loser
+#gamemoves dictionary - player1 moves, player2 moves
