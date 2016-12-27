d307f10c6177b0130b8198c89ff495a0cfe357a2
Nick <nick@hattwick.com>
2016-12-27 03:06:18 -0500

    Add existing file

--- parsing.py
+++ parsing.py
@@ -1,8 +1,9 @@
 import json
 
-def compiler()
+def compiler():
     with open("results.json", 'r') as results:
-        print_this = json.loads(results)
-        print(print_this)
+        datas = json.load(results)
+        numnums = datas["number"]
+        print(numnums)
 
 compiler()
