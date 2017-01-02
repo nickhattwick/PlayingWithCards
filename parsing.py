ddfb200776b69f211d77c2416e590a80c3ecf83f
Nick <nick@hattwick.com>
2017-01-01 22:43:10 -0500

    Add existing file

--- parsing.py
+++ parsing.py
@@ -3,7 +3,7 @@ import json
 with open("results.json", 'r') as results:
         data = json.load(results)
 
-move_ratios = {"Summon" : {}}
+summon_ratios = {}
 
 for turn_log in data["turns"]:
     for moves in turn_log["moves"]:
@@ -12,12 +12,12 @@ for turn_log in data["turns"]:
                 if key == "Summon":
                     print(move)
                     print(move[1])
-                    if move[1] not in move_ratios["Summon"]:
-                        move_ratios["Summon"][move[1]] = {}
+                    if move[1] in summon_ratios:
+                        summon_ratios[move[1]] = {}
                         if data["winner"] == turn_log["player"]:
-                            move_ratios["Summon"][move[1]]["W"] = 1
+                            summon_ratios[move[1]]["W"] = 1
                         elif data["loser"] == turn_log["player"]:
-                            move_ratios["Summon"][move[1]]["L"] = 1
+                            summon_ratios[move[1]]["L"] = 1
 
 with open("parsed.json", 'w') as parsed:
     json.dump(move_ratios, parsed)
