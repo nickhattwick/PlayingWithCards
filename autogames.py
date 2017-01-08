10052c4ac04d055b406d9f8cb94c8ffe524fc90d
Nick <nick@hattwick.com>
2017-01-07 23:00:56 -0600

    Add existing file

---
+++ autogames.py
@@ -0,0 +1,35 @@
+from player import HumanPlayer, AutoPilot
+from prompts import full_turn
+from random import shuffle, choice
+import jsonlog
+
+p1 = AutoPilot("Player 1")
+p2 = AutoPilot("Player 2")
+p1.opponent = p2
+p2.opponent = p1
+
+players = (p1, p2)
+jsonlog.initiate_game(players)
+
+for player in players:
+    shuffle(player.board.deck)
+    for _ in range(7):
+        player.board.draw()
+
+coin_toss = choice([True, False])
+first = p1 if coin_toss else p2
+second = p2 if coin_toss else p1
+
+def all_turns():
+    turn_number = 1
+    while True:
+        yield first, turn_number
+        yield second, turn_number
+        turn_number += 1
+
+keep_playing = True
+for player, turn_number in all_turns():
+    jsonlog.initiate_turn(player, turn_number)
+    keep_playing = full_turn(player)
+    if not keep_playing:
+        break
--- parsing.py
+++ parsing.py
@@ -1,33 +1,24 @@
 import json
+from collections import Counter, defaultdict
 
 with open("results.json", 'r') as results:
         data = json.load(results)
 
-summon_ratios = {}
+summon_ratios = defaultdict(Counter)
 
 for turn_log in data["turns"]:
     for moves in turn_log["moves"]:
         for move in moves:
             for key in move:
                 if key == "Summon":
-                    print(move)
-                    print(move[1])
-                    checkpoint = False
-                    while checkpoint == False:
-                        for summon in summon_ratios:
-                            if summon == move[1]:
-                                if data["winner"] == turn_log["player"]:
-                                    summon_ratios[move[1][0]]["W"] += 1
-                                elif data["loser"] == turn_log["player"]:
-                                    summon_ratios[move[1][0]]["L"] += 1
-                                checkpoint = True
-                        if checkpoint == False:
-                            summon_ratios[move[1][0]] = {}
-                            if data["winner"] == turn_log["player"]:
-                                summon_ratios[move[1][0]]["W"] = 1
-                            elif data["loser"] == turn_log["player"]:
-                                summon_ratios[move[1][0]]["L"] = 1
-                            checkpoint = True
+                    summon = move[1][0]
+                    if data["winner"] == turn_log["player"]:
+                        victory_key = 'W'
+                    elif data["loser"] == turn_log["player"]:
+                        victory_key = 'L'
+                    else:
+                        raise ValueError("player neither won nor lost")
+                    summon_ratios[summon][victory_key] += 1
 
 with open("parsed.json", 'w') as parsed:
     json.dump(summon_ratios, parsed)
