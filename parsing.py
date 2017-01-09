067f9f1a170dfad3178854f5bade810f039f17bc
Nick <nick@hattwick.com>
2017-01-08 18:10:06 -0600

    Add existing file

--- parsing.py
+++ parsing.py
@@ -1,9 +1,6 @@
 import json
 from collections import Counter, defaultdict
 
-with open("results.json", 'r') as results:
-        data = json.load(results)
-
 summon_ratios = defaultdict(Counter)
 
 def get_victory_key(data, player):
@@ -15,15 +12,21 @@ def get_victory_key(data, player):
         raise ValueError("player neither won nor lost")
     return victory_key
 
-for turn_log in data["turns"]:
-    for moves in turn_log["moves"]:
-        for move in moves:
-            for key in move:
-                if key == "Summon":
-                    summon = move[1][0]
-                    player = turn_log["player"]
-                    victory_key = get_victory_key(data, player)
-                    summon_ratios[summon][victory_key] += 1
+def parse_log():
+    with open("results.json", 'r') as results:
+            data = json.load(results)
 
-with open("parsed.json", 'w') as parsed:
-    json.dump(summon_ratios, parsed)
+    for turn_log in data["turns"]:
+        for moves in turn_log["moves"]:
+            for move in moves:
+                for key in move:
+                    if key == "Summon":
+                        summon = move[1][0]
+                        player = turn_log["player"]
+                        victory_key = get_victory_key(data, player)
+                        summon_ratios[summon][victory_key] += 1
+
+    with open("parsed.json", 'w') as parsed:
+        json.dump(summon_ratios, parsed)
+
+parse_log()
