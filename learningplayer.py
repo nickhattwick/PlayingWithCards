183a4af774f5176acd05e20b12a67f036d19267a
Nick <nick@hattwick.com>
2017-01-11 01:53:38 -0600

    Add existing file

--- learningplayer.py
+++ learningplayer.py
@@ -1,7 +1,7 @@
 from player import AutoPilot
 
 class StillLearning(AutoPilot):
-    def get_value(card):
+    def get_card_value(summon):
             try:
                 positive_value = summon["W"]
             except KeyError:
@@ -11,7 +11,8 @@ class StillLearning(AutoPilot):
             except KeyError:
                 negative_value = 0
             value = positive_value - negative_value
-    
+            return value
+
     def auto_summon(self):
         try:
             with open("parsed.json", 'r') as parsed:
@@ -20,41 +21,24 @@ class StillLearning(AutoPilot):
             summon_data = None
 
         current_card = None
+        current_card_value = None
         place = 0
         creatures = [card for card in self.board.hand if card.kind == "creature"]
         while place < len(creatures):
             choice = self.board.hand[place]
+            choice_value = None
             print(choice)
             if not current_card:
                 current_card = choice
                 print("current card: ", current_card)
-            else:
-                current_card_value = None
                 for summon in summon_data:
                     if summon = current_card.name:
-                        try:
-                            positive_value = summon["W"]
-                        except KeyError:
-                            positive_value = 0
-                        try:
-                            negative_value = summon["L"]
-                        except KeyError:
-                            negative_value = 0
-                        current_card_value = positive_value - negative_value
-
-
-
-
-    #def _summon(self):
-        current_card = None
-        place = 0
-        creatures = [card for card in self.board.hand if card.kind == "creature"]
-        while place < len(creatures):
-            choice = self.board.hand[place]
-            print(choice)
-            if not current_card:
-                current_card = choice
-                print("current card: ", current_card)
-            elif (current_card.power < choice.power) and (current_card.cost <= self.board.mana.amount):
-                current_card = choice
-                print("current card: ", current_card)
+                        current_card_value = get_card_value(summon)
+            else:
+                for summon in summon_data:
+                    if summon = choice.name:
+                        choice_value = get_card_value(summon)
+                if choice_value > current_card_value:
+                    current_card = choice
+                    current_card_value = choice_value
+            place += 1
